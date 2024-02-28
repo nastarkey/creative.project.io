3866ff4b85bc486f064330cb04556bec0be9e079
Nathan Starkey <90463278+nastarkey@users.noreply.github.com>
2024-02-28 08:30:30 -0700

    Fix base URL in vite.config.js

--- creative-project/vite.config.js
+++ creative-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "creative.project.io",
+  base: "/creative.project.io",
   plugins: [react()],
 })
